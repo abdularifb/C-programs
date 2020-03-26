d2c3230e6b97c3ee9473d35998ed0f0421ede5b3
abdularifb <61449719+abdularifb@users.noreply.github.com>
2020-03-26 10:16:02 +0530

    Add files via upload

---
+++ centigrate fhranheit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  float c,f;
+  
+  cout<<"Enter the centigrate:";
+  
+  cin>>c;
+  
+  f=(9.0/5) * c + 32;
+  
+  cout<<"fharnheit:"<<f;
+  
+  
+  
+}
+
